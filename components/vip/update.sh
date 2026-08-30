c8050b9e177739f49fcb1b6da9152e0bae180e6b
DIN <dinstoree41@gmail.com>
2026-08-31 01:33:35 +0700

    Create update.sh

---
+++ components/vip/update.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+set -e
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+LOCAL_VIP="${DINSTORE_ROOT:-$(cd "$SCRIPT_DIR/../.." && pwd)}/components/vip"
+dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
+biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
+red() { echo -e "\\033[32;1m${*}\\033[0m"; }
+mkdir -p /usr/local/sbin
+clear
+fun_bar() {
+    CMD[0]="$1"
+    CMD[1]="$2"
+    (
+        [[ -e $HOME/fim ]] && rm $HOME/fim
+        ${CMD[0]} -y >/dev/null 2>&1
+        ${CMD[1]} -y >/dev/null 2>&1
+        touch $HOME/fim
+    ) >/dev/null 2>&1 &
+    tput civis
+    echo -ne "  \033[0;33mPlease Wait Loading \033[1;37m- \033[0;33m["
+    while true; do
+        for ((i = 0; i < 18; i++)); do
+            echo -ne "\033[0;32m#"
+            sleep 0.1s
+        done
+        [[ -e $HOME/fim ]] && rm $HOME/fim && break
+        echo -e "\033[0;33m]"
+        sleep 1s
+        tput cuu1
+        tput dl1
+        echo -ne "  \033[0;33mPlease Wait Loading \033[1;37m- \033[0;33m["
+    done
+    echo -e "\033[0;33m]\033[1;37m -\033[1;32m OK !\033[1;37m"
+    tput cnorm
+}
+res1() {
+   cd
+    cp "$LOCAL_VIP/Cdy/menu.zip" /root/menu.zip
+    install -m 755 "$LOCAL_VIP/Enc/encrypt" /usr/bin/enc
+    7z x -pas123@Newbie menu.zip
+    chmod +x menu/*
+    enc menu/*
+    mv menu/* /usr/local/sbin
+    rm -rf menu
+    rm -rf menu.zip
+    rm -rf *.sh*
+    rm -rf /usr/local/sbin/*~
+    rm -rf /usr/local/sbin/gz*
+    rm -rf /usr/local/sbin/*.bak
+    rm -rf /usr/local/sbin/m-noobz
+    cp "$LOCAL_VIP/Cfg/m-noobz" /root/m-noobz
+    cp /root/m-noobz /usr/local/sbin
+    rm m-noobz*
+    chmod +x /usr/local/sbin/m-noobz
+}
+netfilter-persistent
+clear
+echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e " \e[1;97;101m          UPDATE SCRIPT        \e[0m"
+echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e ""
+echo -e "  \033[1;91m update script service\033[1;37m"
+fun_bar 'res1'
+echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e ""
+echo "Update selesai."
+
+###########- COLOR CODE -##############
